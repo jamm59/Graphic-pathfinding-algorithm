dd6e74a02f560150105019920bb42281d4a4005f
Jam <u02fj21@abdn.ac.uk>
2021-11-22 15:05:11 +0000

    initial2

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+const node = document.querySelector('.node');
+const board = document.querySelector('.board');
+let node_num = 0
 let nodes_on_board = [];
 
-const node = document.querySelector('.node');
+
+let new_node = document.createElement('div');
+new_node.className = 'node';
+board.appendChild(new_node);
