ffdcddeddab51a7396f7637e49697b1ae2814893
Jam <u02fj21@abdn.ac.uk>
2021-11-23 12:40:29 +0000

    js changes

--- main.js
+++ main.js
@@ -1,9 +1,41 @@
-const node = document.querySelector('.node');
+const node = document.querySelectorAll('.node');
 const board = document.querySelector('.board');
 let node_num = 0
 let nodes_on_board = [];
 
+// let new_node = document.createElement('div');
+// new_node.className = 'node';
+// board.appendChild(new_node);
 
-let new_node = document.createElement('div');
-new_node.className = 'node';
-board.appendChild(new_node);
+for (let i=0; i < node.length; i++){
+    node[i].addEventListener('click' ,() => {
+        if (node_num < 2){
+            node[i].style.backgroundColor = 'red';
+            node_num ++;
+        }
+    })
+    
+}
+let BOARD = [
+            [],
+            [],
+            [],
+            [],
+            [],
+            []
+        ]
+
+let count = 0
+let start = 0
+let stop_ = 10
+while (count < BOARD.length){
+    for (let row=start; row < stop_; row++)
+    {
+        BOARD[count].push(node[row]);
+    }
+    temp = start
+    start = stop_
+    stop_ += 10
+    count += 1
+}
+console.log(BOARD);
