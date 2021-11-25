3a4467e051f4aecae58ebd6dd9eb07db1c85c01e
Jam <u02fj21@abdn.ac.uk>
2021-11-25 14:05:23 +0000

    debugging the js code

--- main.js
+++ main.js
@@ -2,12 +2,13 @@
 const node = document.querySelectorAll('.node');
 const board = document.querySelector('.board');
 const start = document.querySelector('.start');
+const addBorder = document.querySelector('.border');
 const btn = document.querySelector('.btn');
 const end = document.querySelector('.stop');
-const start_color = 'rgb(255,0,0)';                     // i used rgb colors because it 
-const stop_color = 'rgb(0,0,0)';                       //  is easier for comparisons
+let start_color = 'rgb(248, 72, 94)';                     // i used rgb colors because it 
+const stop_color =  'rgb(255, 130, 67)';                     //  is easier for comparisons
 const node_color = 'rgb(247, 246, 242)';
-const border_color = '';
+const border_color = 'rgb(0,0,0)'; 
 // non constants
 let node_num = 0;
 let nodes_on_board = [];
@@ -15,25 +16,24 @@ let BOARD = [[],[],[],[],[],[]];
 let started = false;
 let clicked_n = false;
 
-start.addEventListener('click', ()=> {
-    started = !started;
-});
-
 // chosing a start and stop position 
+
+
 for (let i=0; i < node.length; i++){
     node[i].addEventListener('click' ,() => {
-        if (node_num < 2 && !started){
-            clicked_n = !clicked_n;
-            if(clicked_n){
-                node[i].style.backgroundColor = start_color;
-                node_num ++;
-                start_color = stop_color;
-            }else {
-                node[i].style.backgroundColor = node_color;
-                node_num --;
+        // only select a starting and stopping position if 
+        // the started button has not been clicked
+        if (node_num < 2 && !started && !clicked_n){
+            node[i].style.backgroundColor = start_color;
+            start_color = stop_color;
+            node_num ++;
+            if (node_num >= 2){
+                start_color = 'rgb(248, 72, 94)';
+                node_num = 0;
+                clicked_n = true;
             }
         }
-    })
+    });
     
 }
 
@@ -58,60 +58,67 @@ function update_board(){
 function getstart(){
     for(let i=0; i<BOARD.length; i++){
         for(let j=0; j< BOARD[0].length; j++){
-            let n_color = getComputedStyle(BOARD[x1][y1],'backgroundColor').backgroundColor;
-            if (n_color == start_color) {
+            let n_color = getComputedStyle(BOARD[i][j],'backgroundColor').backgroundColor;
+             if (n_color == start_color) {
                 //if it finds the start node return the index 
-                return (i,j);
-            }
+                console.log('row', ' ', i);
+                console.log('column', ' ', j);
+                return [i,j];
+             }
         }
     }
+    return null;
 }
 function isValid(pos){
     let x=pos[0],y=pos[1];
-    color = getComputedStyle(BOARD[x][y],'backgroundColor').backgroundColor;
-    if (color === border_color)
-        return true;
-    return false;
+
+    let color = getComputedStyle(BOARD[x][y],'backgroundColor').backgroundColor;
+    if (color === border_color){
+        return false;
+    }
+    return true;
 }
 function border(pos) {
     let x=pos[0],y=pos[1];
-    if (x > BOARD.length || y > BOARD[0].length || x < 0 || y < 0)
+    if (x > BOARD.length || y > BOARD[0].length || x < 0 || y < 0){
         return true;
+    }  
     return false;
 }
 function find_end(ini_pos){
     // look up down left and right 
-    let x1=ini_pos[0],y1=init_pos[1];
-    let n_color = getComputedStyle(BOARD[x1][y1],'backgroundColor').backgroundColor;
-    if (n_color == stop_color)
+    let x=ini_pos[0];
+    let  y=ini_pos[1];
+    if (getComputedStyle(BOARD[x][y],'backgroundColor').backgroundColor == stop_color){
         return true;
+    }
     direction = [
-        (-1,0),     //top
-        (0,-1),    //left
-		(1,0),    //down
-		(0,1),   //right	
+        [-1,0],     //top
+        [0,-1],    //left
+		[1,0],    //down
+		[0,1],   //right	
 	    ]
     dir_len = direction.length;
-    let x = ini_pos[0];
-    let y = ini_pos[1];
-    //for direction every direction in direction.
+    // for direction every direction in direction.
     for(let i=0; i<dir_len; i++){
         let n_x = x + direction[i][0];
         let n_y = y + direction[i][1];
 
-        if (border((n_x,n_y)) || !isValid((n_x,n_y))) 
+        if (getComputedStyle(BOARD[n_x][n_y],'backgroundColor').backgroundColor == stop_color){
+            return true;
+        }
+
+        if (border([n_x,n_y]) || !isValid([n_x,n_y])) {
+            console.log('we reached here');
             continue
+        }
 
-        if(isValid((n_x,n_y)) && !border((n_x,n_y))){
-
-            let n_color = getComputedStyle(BOARD[n_x][n_y],'backgroundColor').backgroundColor;
-            if (n_color == stop_color) {
-                return true;
-            }
-
-            n_pos = (n_x,n_y);
+        if(isValid([n_x,n_y]) && !border([n_x,n_y])){
+            console.log('did not work');
+            n_pos = [n_x,n_y];
             BOARD[n_pos[0]][n_pos[1]].style.backgroundColor = start_color;
-            update_pos(n_pos);
+            setTimeout(find_end,200,n_pos);
+            
         } 
     }
 }
@@ -120,9 +127,36 @@ function find_end(ini_pos){
 
 function main(pos){
     update_board();
-    find_end(pos);
+    if (find_end(pos)){
+        window.alert('Path found')
+    }
 }
 
 
-let start_pos = getstart();
-//main(start_pos);
+let start_pos = null;
+
+// console.log(start_pos)
+start.addEventListener('click', ()=> {
+    if (clicked_n){
+        started = true;
+    }
+    else {
+        window.alert('Please select a start and stop position first');
+    }
+    if(started){
+        window.alert('started');
+        getstart();
+    }else {
+        window.alert('Unable to start');
+    }
+});
+
+
+addBorder.addEventListener('click', ()=> {
+    update_board();
+    main(getstart());
+})
+end.addEventListener('click',()=>{
+    started = false;
+    node_num = 0;
+});
